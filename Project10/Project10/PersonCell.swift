5a49143582b4b0bbfe6dc67b247bb61f8dbfec24
Lucas Firmo <lucas.firmo@corp.globo.com>
2020-03-29 13:41:33 -0300

    Project 10

---
+++ Project10/Project10/PersonCell.swift
@@ -0,0 +1,16 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by lucas.firmo on 3/29/20.
+//  Copyright © 2020 lucas.firmo. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    
+}

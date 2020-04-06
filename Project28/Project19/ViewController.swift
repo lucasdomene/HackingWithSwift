e61aa9b15234627991bd07a4d4b3db45b7eaa57a
Lucas Firmo <lucas.firmo@corp.globo.com>
2020-04-05 23:50:58 -0300

    Project 28

---
+++ Project28/Project19/ViewController.swift
@@ -0,0 +1,96 @@
+//
+//  ViewController.swift
+//  Project19
+//
+//  Created by lucas.firmo on 4/4/20.
+//  Copyright © 2020 lucas.firmo. All rights reserved.
+//
+
+import UIKit
+import LocalAuthentication
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var secretTextView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Nothing to see here"
+        
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self,
+                                       selector: #selector(adjustForKeyboard),
+                                       name: UIResponder.keyboardWillHideNotification,
+                                       object: nil)
+        notificationCenter.addObserver(self,
+                                       selector: #selector(adjustForKeyboard),
+                                       name: UIResponder.keyboardWillChangeFrameNotification,
+                                       object: nil)
+        notificationCenter.addObserver(self, selector: #selector(saveSecretMessage), name: UIApplication.willResignActiveNotification, object: nil)
+    }
+    
+    @objc func adjustForKeyboard(notification: Notification) {
+        guard let keyboardValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
+        
+        let keyboardScreenEndFrame = keyboardValue.cgRectValue
+        let keyboardViewEndFrame = view.convert(keyboardScreenEndFrame, from: view.window)
+        
+        if notification.name == UIResponder.keyboardWillHideNotification {
+            secretTextView.contentInset = .zero
+        } else {
+            secretTextView.contentInset = UIEdgeInsets(top: 0,
+                                                       left: 0,
+                                                       bottom: keyboardViewEndFrame.height - view.safeAreaInsets.bottom,
+                                                       right: 0)
+        }
+        
+        secretTextView.scrollIndicatorInsets = secretTextView.contentInset
+        
+        let selectedRange = secretTextView.selectedRange
+        secretTextView.scrollRangeToVisible(selectedRange)
+    }
+    
+    @IBAction func authenticateTapped() {
+        let context = LAContext()
+        var error: NSError?
+        
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            let reason = "Identify yourself!"
+            
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { [weak self] success, authenticationError in
+                DispatchQueue.main.async {
+                    if success {
+                        self?.unlockSecretMessage()
+                    } else {
+                        let ac = UIAlertController(title: "Authentication failed", message: "You could not be verified; please try again.", preferredStyle: .alert)
+                        ac.addAction(UIAlertAction(title: "OK", style: .default))
+                        self?.present(ac, animated: true)
+                    }
+                }
+            }
+        } else {
+            let ac = UIAlertController(title: "Biometry unavailable", message: "Your device is not configured for biometric authentication.", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+        }
+    }
+    
+    func unlockSecretMessage() {
+        secretTextView.isHidden = false
+        title = "Secret stuff!"
+        
+        secretTextView.text = KeychainWrapper.standard.string(forKey: "SecretMessage") ?? ""
+    }
+    
+    @objc func saveSecretMessage() {
+        guard !secretTextView.isHidden else { return }
+        
+        KeychainWrapper.standard.set(secretTextView.text, forKey: "SecretMessage")
+        secretTextView.resignFirstResponder()
+        secretTextView.isHidden = true
+        title = "Nothing to see here"
+    }
+    
+}
+
